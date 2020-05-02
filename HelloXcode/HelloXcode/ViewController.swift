9520b012f1f60e37b86e50a07003d802a368d0ab
misseop25 <richard25@naver.com>
2020-05-02 23:45:46 +0900

    Hello_iOS_Project 생성

---
+++ HelloXcode/HelloXcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloXcode
+//
+//  Created by Minseop Kim on 2020/05/02.
+//  Copyright © 2020 Minseop Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
