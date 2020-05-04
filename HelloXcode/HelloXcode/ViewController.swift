b1144842bcf5cf064ac491c09add849249214ba6
misseop25 <richard25@naver.com>
2020-05-04 10:45:04 +0900

    아울렛 및 액션 테스트

--- HelloXcode/HelloXcode/ViewController.swift
+++ HelloXcode/HelloXcode/ViewController.swift
@@ -10,21 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var btn: UIButton!
+    
+    @IBAction func updateLabel(_ sender: Any) {
     }
     
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        btn.backgroundColor = UIColor.red
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
