4f2caf67153d258cd7ffb7bac9d55e9d40026def
misseop25 <richard25@naver.com>
2020-05-20 23:29:27 +0900

    makeLoginProcess 프로젝트 생성

---
+++ makeLogInProcess/makeLogInProcess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  makeLogInProcess
+//
+//  Created by Minseop Kim on 2020/05/20.
+//  Copyright © 2020 Minseop Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
