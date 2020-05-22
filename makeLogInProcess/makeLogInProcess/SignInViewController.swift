45952234b4e76b4bb2fa66fc2dec5e4c37d5cf80
misseop25 <richard25@naver.com>
2020-05-22 17:28:33 +0900

    ID, PS 필드 알럿으로 진행, 아직 문자 체크는 미진행

--- makeLogInProcess/makeLogInProcess/SignInViewController.swift
+++ makeLogInProcess/makeLogInProcess/SignInViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class SignInViewController: UIViewController {
 
+    @IBOutlet weak var checkLabel: UILabel!
+    
+    
+    @IBAction func loginAction(_ sender: Any) {
+        let login = UIAlertController(title: "로그인", message: "아이디와 비밀번호를 입력해주세요.", preferredStyle: .alert)
+        
+        login.addTextField { (idField) in
+            idField.placeholder = "ID"
+        }
+        login.addTextField { (psField) in
+            psField.placeholder = "PS"
+            psField.isSecureTextEntry = true
+        }
+        
+        
+        let okAction = UIAlertAction(title: "확인", style: .default) { (action) in
+            guard let fieldList = login.textFields else {
+                self.checkLabel.text = "ID와 PS를 제대로 입력해주세요"
+                return}
+            guard let idField = fieldList.first else {
+                return self.checkLabel.text = "ID를 입력해주세요"
+                }
+            guard let psField = fieldList.last else {
+                self.checkLabel.text = "PS를 입력해주세요"
+                return}
+        }
+        
+        login.addAction(okAction)
+        present(login, animated: true, completion: nil)
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
