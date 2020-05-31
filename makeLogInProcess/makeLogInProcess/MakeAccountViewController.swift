d5d7beacad4755ab70dee2c5c76dbeca0cefde7a
misseop25 <richard25@naver.com>
2020-05-31 20:39:27 +0900

    Text Field Delegate 연결 및 Outlet 구성

--- makeLogInProcess/makeLogInProcess/MakeAccountViewController.swift
+++ makeLogInProcess/makeLogInProcess/MakeAccountViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 
 class MakeAccountViewController: UIViewController {
 
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var ageField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var phoneField: UITextField!
+    @IBOutlet weak var idField: UITextField!
+    @IBOutlet weak var psField: UITextField!
+    @IBOutlet weak var psCheckField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        nameField.becomeFirstResponder()
+    }
     
 
+}
+extension MakeAccountViewController: UITextFieldDelegate {
+    
+    
 }
