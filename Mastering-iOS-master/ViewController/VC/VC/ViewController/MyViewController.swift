d173b6be663dea00bf4a6b74e4237c844b0b2168
misseop25 <richard25@naver.com>
2021-01-07 22:16:55 +0900

    no message

--- Mastering-iOS-master/ViewController/VC/VC/ViewController/MyViewController.swift
+++ Mastering-iOS-master/ViewController/VC/VC/ViewController/MyViewController.swift
@@ -13,7 +13,6 @@ class MyViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func fromCode(_ sender: Any) {
@@ -23,16 +22,5 @@ class MyViewController: UIViewController {
         navigationController?.pushViewController(vc, animated: true)
     }
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
